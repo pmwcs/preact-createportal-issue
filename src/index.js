eb60faed38b8f4a80f3a1bb4c33277dbc072d4b8
commenthol <commenthol@gmail.com>
2021-01-01 12:19:24 +0100

    demo issue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import './style';
-import { Component } from 'preact';
+import { h } from 'preact';
+import { useEffect, useState, useRef } from 'preact/hooks';
+import { createPortal } from 'preact/compat';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
+const isBrowser = typeof window !== 'undefined';
+
+function Portal ({ id }) {
+	const el = isBrowser ? useRef(document.createElement('div')) : null;
+	return <div ref={el} id={id} />;
+}
+
+function PortalChild ({ children, renderTo }) {
+	const [portalEl, setPortalEl] = useState();
+
+	useEffect(() => {
+		if (isBrowser && renderTo) {
+			const el = document.getElementById(renderTo);
+			setPortalEl(el);
+		}
+	}, [renderTo, portalEl]);
+
+	if (portalEl)
+		return createPortal(children, portalEl);
+	return null;
+}
+
+export default function App (){
+	return (
+		<div>
+			<div style={{ position: 'relative', width: '10em', left: '5em', border: '1px dashed fuchsia', color: 'fuchsia' }}>
+					Portal
+				<Portal id="portal" />
 			</div>
-		);
-	}
+			<div>Render outside Portal</div>
+			<PortalChild renderTo="portal">
+				<div>Render in Portal</div>
+			</PortalChild>
+		</div>
+	);
 }
